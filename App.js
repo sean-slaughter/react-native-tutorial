29f3a68eab24a0f1b00ec20587e0d9748c4e7636
sean-slaughter <sthompson2822@gmail.com>
2020-12-27 15:17:14 -0600

    Create app folder and screens folder

--- App.js
+++ App.js
@@ -2,46 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
 
   console.log(useDeviceOrientation())
 
   return (
-    <View
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          right: -20,
-          top:20,
-          position: "absolute"
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100
-        }}
-      />
-    </View>
+    <WelcomeScreen/>
   );
 }
 
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground 
+        source={require('../assets/background.jpg')}
+        style={styles.background}
+        >
+        
+
+
+        </ImageBackground>
+    );
+}
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+
+    }
+})
+export default WelcomeScreen;
