a93c74ecce3271b1eee26e321ef82e11da4cff2d
sean-slaughter <sthompson2822@gmail.com>
2020-12-28 17:24:35 -0600

    Encapsulate text styles in AppText component

--- App.js
+++ App.js
@@ -1,29 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
-import ViewImageScreen from './app/screens/ViewImageScreen';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, StyleSheet, Dimensions, Text } from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
+
 
 export default function App() {
-
-
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
-      }}>
-        <View style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          borderWidth: 10,
-          borderColor: "royalblue",
-          borderRadius: 50
-        }}>
+        alignItems: "center",
+      }}
+    >
+      <AppText>I love react native!</AppText>
+      <Text
+        style={{
+          fontFamily: "Roboto",
+          fontSize: 30,
+          fontStyle: "italic",
+          fontWeight: "bold",
+          textAlign: "center"
+        }}
+      >
+       THIS IS MY FIRST REACT NATIVE APP
+        HERES SOME MORE TEXT
+      </Text>
 
-        </View>
     </View>
   );
 }
@@ -31,14 +39,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white'
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
   },
 
   logo: {
     width: 300,
     height: 200,
-  }
+  },
 });
