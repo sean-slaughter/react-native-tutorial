d82aa67b0f4adb3e0b83f3a26f7521ab2313ce4b
sean-slaughter <sthompson2822@gmail.com>
2020-12-28 16:51:12 -0600

    Add borders to app view

--- App.js
+++ App.js
@@ -8,7 +8,23 @@ export default function App() {
 
 
   return (
-    <ViewImageScreen/>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <View style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50
+        }}>
+
+        </View>
+    </View>
   );
 }
 
